bbcfdf0d00b6c2cc985ec2ae881511e19c5f968d
kongkrit <45794368+kongkrit@users.noreply.github.com>
2025-12-03 03:32:40 +0700

    update code

---
+++ code/007-nmi_handler.c
@@ -0,0 +1,9 @@
+#include <stdint.h>
+
+#define NMI_COUNT 0x0064U
+
+void nmi_handler (void) __critical __interrupt {
+  volatile uint8_t *count = (volatile uint8_t *) NMI_COUNT;
+
+  *count = *count + 1;
+}
