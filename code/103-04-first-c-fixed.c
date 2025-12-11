baf19a31cbc8011ad814e7a872b9960aa41fce2f
kongkrit <45794368+kongkrit@users.noreply.github.com>
2025-12-12 00:18:56 +0700

    comply with coding guideline and style

--- code/103-04-first-c-fixed.c
+++ code/103-04-first-c-fixed.c
@@ -1,12 +1,13 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#define M_ADDR 0x4000
+#define M_ADDR ((int16_t *)0x4000U)
 
-void main(void) {
+void main (void) {
     int16_t a;
     a = -27;
     int16_t b = 54;
-    int16_t *m = (int16_t *) M_ADDR;
-    *m = a * b;
+    int16_t *p = M_ADDR;
+
+    *p = a * b;
 }
